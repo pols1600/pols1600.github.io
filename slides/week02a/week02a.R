956c628c47c19ea86aa54a6e7912dd8fb0426d0e
Danilo Freire <47090708+pols1600@users.noreply.github.com>
2019-01-28 00:18:03 -0500

    Add files via upload

---
+++ slides/week02a/week02a.R
@@ -0,0 +1,16 @@
+##########################################
+# POLS 1600 - Political Research Methods #
+# Week 02a - Introduction to R           #
+# Danilo Freire                          #
+##########################################
+
+## Recap
+2^5
+vec <- seq(from = 1, to = 100, by = 2)
+summary(vec)
+install.packages("congressbr")
+library("swirl")
+swirl()
+# R won't read this line
+
+## 
